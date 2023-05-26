26a031c045ffa7070e47b1e3a84182310ec10535
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-05-26 14:39:16 +0300

    implemented_scss_to_multicheckbox

--- src/components/selectDropBox.vue
+++ src/components/selectDropBox.vue
@@ -12,19 +12,13 @@ const emit = defineEmits(['checkBoxChange'])
 const checkBoxValues = ref([])
 const uniqueCheckboxId = `list ${Math.random() * 1e18}`
 
-onUpdated(() => emit('checkBoxChange', JSON.stringify(checkBoxValues.value)))
-
 const { formSubmitCount } = storeToRefs(store)
-
-watch(formSubmitCount, () => (checkBoxValues.value.length = 0))
 
 const dropDownHide = () => {
   const checkList = document.getElementById(uniqueCheckboxId)
   if (checkList?.classList.contains('visible'))
     checkList.classList.remove('visible')
 }
-
-document.querySelector('.tabs').addEventListener('click', () => dropDownHide())
 
 const dropDownToggle = (e) => {
   const checkList = document.getElementById(uniqueCheckboxId)
@@ -35,6 +29,12 @@ const dropDownToggle = (e) => {
 }
 
 const stopListHideOnclick = (e) => e.stopPropagation()
+
+document.querySelector('.tabs').addEventListener('click', () => dropDownHide())
+
+onUpdated(() => emit('checkBoxChange', JSON.stringify(checkBoxValues.value)))
+
+watch(formSubmitCount, () => (checkBoxValues.value.length = 0))
 </script>
 <template>
   <div :id="uniqueCheckboxId" class="dropdown-check-list" tabindex="100">
@@ -57,70 +57,71 @@ const stopListHideOnclick = (e) => e.stopPropagation()
     </ul>
   </div>
 </template>
-<style>
+<style lang="scss">
 .dropdown-check-list {
   display: inline-block;
   grid-column-start: 4;
   grid-column-end: 9;
   position: relative;
+
+  .anchor {
+    position: relative;
+    cursor: pointer;
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    border: 1px solid #ccc;
+    line-height: 2.5rem;
+    padding-left: 0.25rem;
+    border-radius: 0.25rem;
+
+    &:after {
+      position: absolute;
+      content: '';
+      border-left: 2px solid black;
+      border-top: 2px solid black;
+      padding: 5px;
+      right: 10px;
+      top: 30%;
+      -moz-transform: rotate(-135deg);
+      -ms-transform: rotate(-135deg);
+      -o-transform: rotate(-135deg);
+      -webkit-transform: rotate(-135deg);
+      transform: rotate(-135deg);
+    }
+
+    &:active:after {
+      right: 10px;
+      top: 30%;
+    }
+  }
+
+  ul.items {
+    padding: 2px;
+    width: 100%;
+    display: none;
+    margin: 0;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    border-top: none;
+    position: absolute;
+    background-color: white;
+
+    li {
+      list-style: none;
+      width: 100%;
+    }
+  }
 }
 
-.dropdown-check-list .anchor {
-  position: relative;
-  cursor: pointer;
-  display: inline-block;
-  width: 100%;
-  height: 100%;
-  border: 1px solid #ccc;
-  line-height: 2.5rem;
-  padding-left: 0.25rem;
-  border-radius: 0.25rem;
-}
-
-.dropdown-check-list .anchor:after {
-  position: absolute;
-  content: '';
-  border-left: 2px solid black;
-  border-top: 2px solid black;
-  padding: 5px;
-  right: 10px;
-  top: 30%;
-  -moz-transform: rotate(-135deg);
-  -ms-transform: rotate(-135deg);
-  -o-transform: rotate(-135deg);
-  -webkit-transform: rotate(-135deg);
-  transform: rotate(-135deg);
-}
-
-.dropdown-check-list .anchor:active:after {
-  right: 10px;
-  top: 30%;
-}
-
-.dropdown-check-list ul.items {
-  padding: 2px;
-  width: 100%;
-  display: none;
-  margin: 0;
-  border: 1px solid #ccc;
-  border-radius: 0.25rem;
-  border-top: none;
-  position: absolute;
-  background-color: white;
-}
-
-.dropdown-check-list ul.items li {
-  list-style: none;
-  width: 100%;
-}
-
-.dropdown-check-list.visible .anchor {
-  color: #0094ff;
-  border: black solid 3px;
-}
-
-.dropdown-check-list.visible .items {
-  display: block;
-  z-index: 10;
+.dropdown-check-list.visible {
+  .anchor {
+    color: #0094ff;
+    border: black solid 3px;
+  }
+  .items {
+    display: block;
+    z-index: 10;
+  }
 }
 </style>
